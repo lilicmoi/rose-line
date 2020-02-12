19d6787611e15f1e2efd5143d68a206d2fdc023d
MYBE <56382778+lilicmoi@users.noreply.github.com>
2020-02-12 16:37:56 +0100

    classe chambre hotel

---
+++ exam/src/main/java/com/example/exam/Chambre.java
@@ -0,0 +1,37 @@
+package com.example.exam;
+
+//room class
+public class Chambre {
+  private  int idRoom;
+  private  int etaj;
+  private boolean vide;
+  public Chambre()
+  {
+      this.idRoom = 0;
+      this.etaj = 0;
+      this.vide = false;
+  }
+  public Chambre(int idRoom, int etaj, boolean empty) {
+      this.idRoom = idRoom;
+      this.etaj = etaj;
+      this.vide = empty;
+  }
+  public int getEtaj() {
+      return etaj;
+  }
+  public void setEtaj(int etaj) {
+      this.etaj = etaj;
+  }
+  public boolean estVide() {
+      return vide;
+  }
+  public void setVide(boolean vide) {
+      this.vide = vide;
+  }
+  public int getIdRoom() {
+      return idRoom;
+  }
+  public void setIdRoom(int idRoom) {
+      this.idRoom = idRoom;
+  }
+}
